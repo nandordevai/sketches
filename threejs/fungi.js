f33130345ac4ae597d3e635d1446183ff3ab8584
Nandor Devai <mail@nandordevai.net>
2021-03-09 17:56:41 +0100

    add fog, max connections

--- threejs/fungi.js
+++ threejs/fungi.js
@@ -2,6 +2,7 @@ const containerSize = 40;
 const maxDistance = 8;
 const n = 150;
 const curveDisplacement = 3;
+const maxConnections = 3;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -11,18 +12,21 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.z = containerSize + 10;
 camera.lookAt(0, 0, 0);
 
-const ambientLight = new THREE.AmbientLight();
-ambientLight.color.setHSL(0, 0, 0.4);
 const directionalLight = new THREE.DirectionalLight();
-directionalLight.color.setHSL(0, 0, .8);
+directionalLight.color.setHSL(0, 0, .3);
 directionalLight.position.set(0, 3, 8);
+
+const fog = new THREE.Fog();
+fog.color.setHSL(0, 0, 0);
+fog.near = containerSize / 3;
+fog.far = containerSize * 2.25
 
 const scene = new THREE.Scene();
 scene.add(directionalLight);
-scene.add(ambientLight);
 
 const container = new THREE.Object3D();
 scene.add(container);
+scene.fog = fog;
 
 class Node {
     constructor() {
@@ -35,7 +39,9 @@ class Node {
     }
 
     setNeighbours(nodes) {
-        this.neighbours = nodes.filter(this.isClose.bind(this));
+        this.neighbours = nodes
+            .filter(this.isClose.bind(this))
+            .slice(0, maxConnections);
     }
 
     isClose(node) {
@@ -44,8 +50,6 @@ class Node {
     }
 
     drawConnections() {
-        const m = new THREE.MeshLambertMaterial();
-        m.color.setHSL(.2, .5, .5);
         this.neighbours.forEach((_) => {
             const mid = new THREE.Vector3();
             mid.copy(this.position);
@@ -61,17 +65,28 @@ class Node {
                 mid,
                 _.position
             ]);
-            const g = new THREE.TubeGeometry(curve, 20, .15, 5, false);
-            const mesh = new THREE.Mesh(g, m);
+            const g = new THREE.TubeGeometry(curve, 20, .15, 5, true);
+            const mesh = new THREE.Mesh(g, material);
             container.add(mesh);
         });
     }
 }
 
+const material = new THREE.MeshLambertMaterial();
+const color = new THREE.Color();
+color.setHSL(.15, .8, .5);
+material.emissive = color;
 const nodes = Array.from({ length: n }, () => new Node());
+const g = new THREE.SphereGeometry(.3, 16, 16);
 nodes.forEach((_) => {
     _.setNeighbours(nodes);
-    _.drawConnections();
+    // TODO: remove duplicate connections
+    if (_.neighbours.length) {
+        const mesh = new THREE.Mesh(g, material);
+        mesh.position.copy(_.position);
+        container.add(mesh);
+        _.drawConnections();
+    }
 });
 
 function render(time) {
